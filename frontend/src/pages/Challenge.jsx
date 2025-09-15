089d719da8c44098515745029c30cd931cc8d2d7
Guilherme Fraga <gui.fraga2009@hotmail.com>
2025-09-15 17:27:26 -0300

    Fixes on href to download challenge

--- frontend/src/pages/Challenge.jsx
+++ frontend/src/pages/Challenge.jsx
@@ -15,7 +15,9 @@ export default function Challenge() {
     <div style={{ padding: "2rem" }}>
       <h1>Bem-vindo(a), {name} à página do Desafio Técnico</h1>
       <p>Página comum para todos os participantes.</p>
-      <Button href="${import.meta.env.VITE_API_URL/challenge/download" variant="contained">
+      <Button href={`${import.meta.env.VITE_API_URL}/challenge/download`}
+              variant="contained"
+      >
         Baixar Desafio
       </Button>
       <br />
